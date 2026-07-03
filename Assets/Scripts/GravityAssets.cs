58f634916594bba6b0947a1081450ad1ef8b969a
琼汐音 <3475238125@qq.com>
2026-07-03 23:00:33 +0800

    添加planet assets

---
+++ Assets/Scripts/GravityAssets.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GravityAssets : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Gravity Assets")]
+    [SerializeField] private float gravityScale = 10.0f;
+    public float GravityScale => gravityScale;
+   public Vector2 GetGravityDirection(Vector2 objectPosition)
+    {
+        return ((Vector2)transform.position - objectPosition).normalized;
+    }
+    public Vector2 GetMovementDirection(Vector2 objectPosition)
+    {
+        return new Vector2(-GetGravityDirection(objectPosition).y, GetGravityDirection(objectPosition).x);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
